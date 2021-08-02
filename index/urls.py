2b4014dc8c8200e4342e8c838c5af2734e4c4963
SmritySarkar <smritysarkarsmrity@gmail.com>
2021-08-03 00:05:20 +0600

    sign-up page added

--- index/urls.py
+++ index/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from index import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home', views.index, name='index'),
     path('register', views.register, name='register'),
 
 
